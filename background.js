b5c990691fa322d0f5f525f80aefbf5b0faa623a
Matthew M <myself123@live.com>
2017-05-16 23:55:15 -0400

    changed image tab recogition to recognize more images

--- background.js
+++ background.js
@@ -29,7 +29,19 @@ function downloadAllImageTabs()
 
 function isImageTab(tab)
 {
-    let url = tab.url.toLowerCase();
+    // this should allow finding images with extra stuff added to them
+    // like i.imgur.com/foo.jpg?maxres=default
+    // 
+    // There is a better way, because firefox knows when an image is an image
+    // the title of that tab will change to
+    // <title> (XXX IMAGE)
+    // Why can't I have this information?
+    // This is hacky BS and everyone knows it.
+    //
+    // Note also that if a tab has been 'zombified'
+    // this function will report a false-negative, because zombie
+    // tabs always report 'about:blank'
+    let url = new URL(tab.url).pathname.toLowerCase();
 
     return url.endsWith(".png")
         || url.endsWith(".jpeg")
